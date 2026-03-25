361ad44d3446cedde259a6665933a3ddd9772c9e
Nildaproduction <nildapublishing@gmail.com>
2026-03-25 01:41:40 +0100

    Redirect from Home to Store page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
-'use client'
+import { redirect } from 'next/navigation';
 
-export default function StorePage() {
-  return (
-    <div className="w-full min-h-screen">
-      <iframe
-        src="https://tikiziki.vercel.app/store"
-        className="w-full h-screen border-none"
-        title="TIKIZIKI Store"
-      />
-    </div>
-  )
+export default function Home() {
+  redirect('/store');
 }
